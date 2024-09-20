2b9c5969f9bd21e774660247e683f5bb5f58e982
spelkey-ucd <spelkey@ucdavis.edu>
2024-09-20 12:51:22 -0400

    update pg class

--- simple-spa/src/lib/db-models/pg.js
+++ simple-spa/src/lib/db-models/pg.js
@@ -70,13 +70,62 @@ class Pg {
     return this._toEqualsClause(queryObject, ', ');
   }
 
-  _toEqualsClause(queryObject, sep=' AND '){
+  /**
+   * @description Converts an object to parameters of an INSERT clause
+   * @param {Object} obj - key value pairs for clause
+   * @returns {Object} {keys: ['foo', 'bar'], values: ['fooValue', 'barValue'], placeholders: ['$1', '$2']}
+   */
+  prepareObjectForInsert(obj){
+    const out = {keys: [], values: [], placeholders: []};
+    for (const k in obj) {
+      out.keys.push(k);
+      out.values.push(obj[k]);
+      out.placeholders.push(`$${out.values.length}`);
+    }
+
+    out.keysString = out.keys.join(', ');
+    out.valuesString = out.values.join(', ');
+    out.placeholdersString = out.placeholders.join(', ');
+    return out;
+  }
+
+  _toEqualsClause(queryObject, sep=' AND ', indexStart=0){
     let sql = '';
     const values = [];
     if ( queryObject && typeof queryObject === 'object' ){
-      for (const [i, k] of (Object.keys(queryObject)).entries()) {
-        values.push(queryObject[k]);
-        sql += `${i > 0 ? sep : ''}${k}=$${i+1}`;
+      let i = indexStart;
+      for (const k of Object.keys(queryObject)) {
+        // make an IN clause if the value is an array
+        if ( Array.isArray(queryObject[k]) ){
+          const inClause = queryObject[k].map((v, j) => `$${i + j + 1}`).join(', ');
+          values.push(...queryObject[k]);
+          sql += `${i > indexStart ? sep : ''}${k} IN (${inClause})`;
+          i += queryObject[k].length;
+
+        // if the value is an object with an operator key, use that operator
+        } else if ( queryObject[k] && typeof queryObject[k] === 'object' && queryObject[k].operator && queryObject[k].value !== undefined){
+          const operator = queryObject[k].operator;
+          const value = queryObject[k].value;
+          values.push(value);
+          sql += `${i > indexStart ? sep : ''}${k} ${operator} $${i+1}`;
+          i++;
+
+        // if the value is an object without a value key, treat it as nested and recurse. check for relation key
+        } else if ( queryObject[k] && typeof queryObject[k] === 'object' && queryObject[k].relation !== undefined ){
+          const q = {...queryObject[k]};
+          const relation = q.relation;
+          delete q.relation;
+          const nested = this._toEqualsClause(q, relation ? ` ${relation} ` : sep, i);
+          values.push(...nested.values);
+          sql += `${i > indexStart ? sep : ''}(${nested.sql})`;
+          i += nested.values.length;
+
+        // else make an equals clause
+        } else {
+          values.push(queryObject[k]);
+          sql += `${i > indexStart ? sep : ''}${k}=$${i+1}`;
+          i++;
+        }
       }
     }
     return {sql, values};
