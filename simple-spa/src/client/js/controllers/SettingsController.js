5257c13bf0f5fa402c0d4c0e34da1f11a84158dc
spelkey-ucd <spelkey@ucdavis.edu>
2024-10-09 16:40:51 -0400

    settings controller

--- simple-spa/src/client/js/controllers/SettingsController.js
+++ simple-spa/src/client/js/controllers/SettingsController.js
@@ -1,10 +1,48 @@
+import { getLogger } from '@ucd-lib/cork-app-utils';
+
 export default class SettingsController {
-  constructor(host, category, categoryConfig) {
+  constructor(host, category, propertyMapper) {
+    this.logger = getLogger(`SettingsController-${host.constructor.name}-${category}`);
     this.host = host;
     this.category = category;
     this.categoryConfig = categoryConfig;
+    this.propertyMapper = propertyMapper;
     host.addController(this);
-    this._initModelCallback();
+    this._init();
+  }
+
+  /**
+   * @description Set the property mapper, which maps settings to properties on this class.
+   * @param {Array} propertyMapper - Array of setting name strings or objects with the following properties:
+   * - setting: setting name
+   * - property: property name to set on this class
+   * - defaultValue: default value if setting is not found
+   */
+  setPropertyMapper(propertyMapper=[]){
+    const defaultValue = '';
+    this.propertyMapper = propertyMapper.map(item => {
+      if ( typeof item === 'string' ) {
+        let property = this._toCamelCase(item);
+        return {setting: item, property, defaultValue};
+      }
+      if ( !item.property ) {
+        item.property = this._toCamelCase(item.setting);
+      }
+      if ( item.defaultValue === undefined ) {
+        item.defaultValue = defaultValue;
+      }
+      return item;
+
+    });
+  }
+
+  /**
+   * @description Convert a string to camel case
+   * @param {String} str
+   * @returns
+   */
+  _toCamelCase(str){
+    return str.replace(/[-.]/g, '_').replace(/_([a-z])/g, (g) => g[1].toUpperCase());
   }
 
   /**
@@ -18,14 +56,34 @@ export default class SettingsController {
   /**
    * @description Wire up the controller to listen to the SettingsModel updates
    */
-  _initModelCallback() {
+  _init() {
+
+    if ( this.host['_onSettingsGetByCategoryUpdate'] ){
+      this.logger.debug('Host callback already exists, not setting up again');
+      return;
+    }
+
+    this.logger.debug('Setting up host callback');
     this.host['_onSettingsGetByCategoryUpdate'] = (e) => this._onSettingsUpdate(e);
     this.host._injectModel('SettingsModel');
+
+    // check if settings are already loaded and call callback if they are
+    const data = this.host.SettingsModel.store.data.getByCategory.get(this.category);
+    if ( data?.state === 'loaded' ) {
+      this.logger.debug('Settings already loaded, calling callback');
+      this._onSettingsUpdate(data);
+    } else {
+      // set default host properties
+      this.propertyMapper.forEach(item => {
+        this[item.property] = item.defaultValue;
+      });
+      this.host.requestUpdate();
+    }
   }
 
   _onSettingsUpdate(e){
     if ( e.state !== 'loaded' || e.settingsCategory != this.category ) return;
-    console.log('SettingsController._onSettingsUpdate', e);
+    this.logger.info('Settings loaded', e);
 
   }
 }
